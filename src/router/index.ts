8b31dc253f00358931cabbadd26bb842d022d7a5
釋明丶 <3352711163@qq.com>
2024-09-28 11:06:46 +0800

    proj init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,12 @@
+// 这是默认生成的路由管理文件
+// 我未作大量修改
+
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import "../views/"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
