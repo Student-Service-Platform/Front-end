15d586e58bb405a5d96a617a60985a150c489070
D4C-h <3753763584@qq.com>
2024-10-05 19:40:09 +0800

    Update PasswordResetView.vue

--- Common/src/views/PasswordResetView.vue
+++ Common/src/views/PasswordResetView.vue
@@ -1,10 +1,48 @@
-<script setup lang="ts">
+<script lang="ts">
 import CusInput from '@/ui/CusInput.vue'
 import CusButton from '@/ui/CusButton.vue'
 import AugmentedReality from '@/assets/AugmentedReality.png'
 import password from '@/assets/password.png'
 import Mail from '@/assets/Mail.png'
 import code from '@/assets/code.png'
+import { resetpwd } from '@/apis/src/resetpassword';
+
+export default {
+  data() {
+    return {
+      AugmentedReality,
+      password,
+      Mail,
+      code,
+      userId: '',           // 学号
+      email: '',            // 邮箱
+      verificationCode: '', // 验证码
+      passwordInput: '',    // 密码
+      confirmPassword: '',  // 确认密码
+      resetpwd
+    }
+  },
+  components: {
+    CusButton,
+    CusInput
+  },
+  methods: {
+    async resetpassword() {
+      if (this.passwordInput !== this.confirmPassword) {
+        alert("两次输入的密码不一致");
+        return;
+      }
+      try {
+        const response = await resetpwd(this.userId, this.email, this.verificationCode, this.passwordInput, this.confirmPassword);
+        alert("密码修改成功！");
+      } 
+      catch (error) {
+        alert("修改失败，请检查输入的信息。");
+        console.error(error);
+      }
+    }
+  }
+}
 </script>
 
 <template>
@@ -15,6 +53,7 @@ import code from '@/assets/code.png'
       class="input"
       :path="AugmentedReality"
       content="请输入ID"
+      v-model="userId"
       required
     ></cus-input>
     <cus-input
@@ -22,6 +61,7 @@ import code from '@/assets/code.png'
       class="input"
       :path="Mail"
       content="请输入邮箱"
+      v-model="email"
       required
     ></cus-input>
     <cus-input
@@ -30,6 +70,7 @@ import code from '@/assets/code.png'
       :path="code"
       content="请输入验证码"
       width="200px"
+      v-model="verificationCode"
       required
     ></cus-input>
     <cus-input
@@ -37,6 +78,8 @@ import code from '@/assets/code.png'
       class="input"
       :path="password"
       content="请输入密码"
+      v-model="passwordInput"
+      type="password"
       required
     ></cus-input>
     <cus-input
@@ -44,12 +87,15 @@ import code from '@/assets/code.png'
       class="input"
       :path="password"
       content="请再次输入密码"
+      v-model="confirmPassword"
+      type="password"
       required
     ></cus-input>
-    <cus-button class="PasswordReset-btn" content="一键修改"></cus-button>
+    <cus-button class="PasswordReset-btn" content="一键修改" @click="resetpassword"></cus-button>
     <cus-button class="code-btn" content="发送验证码"></cus-button>
   </div>
 </template>
+
 <style scoped>
 .PasswordReset {
   border-radius: 16px;
