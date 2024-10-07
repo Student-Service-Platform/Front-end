8f36f1b7d303696922441020ae4e8b74a8e9d816
釋明丶 <3352711163@qq.com>
2024-10-07 14:33:50 +0800

    中间件完善

--- Common/src/router/guard.ts
+++ Common/src/router/guard.ts
@@ -1,4 +1,4 @@
-import { postUser } from '@/apis/src/guard'
+import { postUser, postAdmin, postSuper } from '@/apis/src/guard'
 import type { RouteLocationNormalizedGeneric } from 'vue-router'
 import cookies from "vue-cookies";
 
@@ -8,6 +8,52 @@ export const UserGuard = (
   next: Function
 ) => {
   postUser().then(res=>{
+    if (res.data.code == 200401) { // 登录信息无效或未登录
+      localStorage.clear()
+      cookies.remove("user_id")
+      next({name: "login"})
+    } else if (res.data.code == 200403) {
+        alert("无权访问")
+        next(from)
+      }
+    else next()
+  }).catch(err=>{
+    console.log(err)
+    localStorage.clear()
+    cookies.remove("user_id")
+    next({name: "login"})
+  })
+}
+
+export const AdminGuard = (
+  to: RouteLocationNormalizedGeneric,
+  from: RouteLocationNormalizedGeneric,
+  next: Function
+) => {
+  postAdmin().then(res=>{
+    if (res.data.code == 200401) { // 登录信息无效或未登录
+      localStorage.clear()
+      cookies.remove("user_id")
+      next({name: "login"})
+    } else if (res.data.code == 200403) {
+        alert("无权访问")
+        next(from)
+      }
+    else next()
+  }).catch(err=>{
+    console.log(err)
+    localStorage.clear()
+    cookies.remove("user_id")
+    next({name: "login"})
+  })
+}
+
+export const SuperGuard = (
+  to: RouteLocationNormalizedGeneric,
+  from: RouteLocationNormalizedGeneric,
+  next: Function
+) => {
+  postSuper().then(res=>{
     if (res.data.code == 200401) { // 登录信息无效或未登录
       localStorage.clear()
       cookies.remove("user_id")
