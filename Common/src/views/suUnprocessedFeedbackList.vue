4b14572d0140a0d74a99b9b92bca1c7e15481bb6
jessiexichen <110716927+jessiexichen@users.noreply.github.com>
2024-10-07 11:06:33 +0800

    超管 已处理 未处理反馈 list和expansio逻辑 无router 跳转

--- Common/src/views/suUnprocessedFeedbackList.vue
+++ Common/src/views/suUnprocessedFeedbackList.vue
@@ -1,24 +1,42 @@
-<!-- 超级管理员 未处理反馈列表 -->
-
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import CusBox from '@/ui/CusBox.vue';         // 引入 CusBox 组件
 import CusColumn from '@/ui/CusColumn.vue';   // 引入 CusColumn 组件
 import CusButton2 from '@/ui/CusButton2.vue';
+import { getUnFeedbacks } from '@/apis/src/admin';  // 引入封装的 API 方法
+import router from '@/router';
 
-// 模拟数据
-const rows = Array(20).fill({
-  number: "",
-  id: "",
-  title: "",
-  time: "",
-  category: "",
-  urgency: "",
-  anonymous: "",
-  progress: "",
-  action: "查看" 
+// 定义 rows 数组来存储获取到的反馈数据
+const rows = ref<Array<any>>([]);  // 使用 ref 来响应式存储数据
+const code = ref<number>(0);       // 用于存储反馈序号
+// 定义获取反馈数据的函数
+const fetchFeedbacks = async () => {
+  try {
+    const response = await getUnFeedbacks(0, 20);
+    console.log(response)
+    if (response.data.code == 200200)
+    // 假设 response.data 是包含反馈信息的数组
+      rows.value = response.data.data;  // 将反馈数据存储在 rows 中
+    else {
+      alert(response.data.msg)
+      setInterval(()=>{
+        router.push({name:"view"})
+      }, 5000)
+    }
+  } catch (error) {
+    console.error('获取反馈数据失败:', error);
+  }
+};
+
+// 在组件挂载时获取反馈数据
+onMounted(() => {
+  fetchFeedbacks();
 });
-</script>
 
+function jump(id:number) {
+  router.push({name: "unExp", query: { id }})
+}
+</script>
 <template>
   <header>
     <!-- 左侧菜单栏 -->
@@ -31,6 +49,7 @@ const rows = Array(20).fill({
     ]" :default="1"></cus-column>
   </header>
 
+
   <!-- 右侧表格 -->
   <div style="position:absolute; left:400px; top:70px;">
     <!-- 列头，包含“标题”到“操作” -->
@@ -40,30 +59,32 @@ const rows = Array(20).fill({
       <cus-box :input="false" content="标题" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
       <cus-box :input="false" content="时间" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
       <cus-box :input="false" content="问题类别" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
-      <cus-box :input="false" content="是否紧急" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
+      <cus-box :input="false" content="紧急度" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
       <cus-box :input="false" content="是否匿名" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
       <cus-box :input="false" content="操作" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
     </div>
 
-    <!-- 生成20行数据 -->
+    <!-- 根据 rows 数据动态生成反馈记录 -->
     <div v-for="(row, index) in rows" :key="index" style="display: flex;">
-      <cus-box :input="false" :content="row.number" h="60px" w="129px" :textAlign="['center', 'top']" style="background-color: rgb(255, 255, 255);"></cus-box>
-      <cus-box :input="false" :content="row.id" h="60px" w="129px" :textAlign="['center', 'top']" style="background-color: rgb(255, 255, 255);"></cus-box>
-      <cus-box :input="false" :content="row.title" h="60px" w="129px" :textAlign="['center', 'top']" style="background-color: rgb(255, 255, 255);"></cus-box>
-      <cus-box :input="false" :content="row.time" h="60px" w="129px" :textAlign="['center', 'top']" style="background-color: rgb(255, 255, 255);"></cus-box>
-      <cus-box :input="false" :content="row.category" h="60px" w="129px" :textAlign="['center', 'top']" style="background-color: rgb(255, 255, 255);"></cus-box>
-      <cus-box :input="false" :content="row.urgency" h="60px" w="129px" :textAlign="['center', 'top']" style="background-color: rgb(255, 255, 255);"></cus-box>
-      <cus-box :input="false" :content="row.anonymous" h="60px" w="129px" :textAlign="['center', 'top']" style="background-color: rgb(255, 255, 255);"></cus-box>
-      <cus-box :input="false" h="60px" w="129px" style="background-color: rgb(255, 255, 255);">
-        <cus-button2 content="查看" style="left:30.5px;top:19.3px;"></cus-button2>
+      <!-- 使用顶层的 code 来显示反馈序号 -->
+      <cus-box :input="false" :content="code" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
+      <cus-box :input="false" :content="row.id" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
+      <cus-box :input="false" :content="row.title" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
+      <cus-box :input="false" :content="row.created_at" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
+      <cus-box :input="false" :content="row.category" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
+      <cus-box :input="false" :content="row.urgency" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
+      <cus-box :input="false" :content="row.if_anonymous ? '是' : '否'" h="60px" w="129px" :textAlign="['center', 'top']"></cus-box>
+      <cus-box :input="false" h="60px" w="129px">
+        <cus-button2 content="查看" style="left:30.5px;top:19.3px;" :click="()=>{jump(row.id)}"></cus-button2>
       </cus-box>
-      
     </div>
   </div>
 
   <RouterView />
 </template>
 
+
 <style scoped>
 /* 去除列与列之间的间隙 */
 </style>
+
--- Common/src/views/suUnrocessedFeedbackExpansion.vue
+++ Common/src/views/suUnrocessedFeedbackExpansion.vue
@@ -1,6 +1,77 @@
 <!-- 超管 未处理（接单）反馈拓展 -->
+<script lang="ts">
+// 引入自定义组件和图片资源
+import CusBox from '@/ui/CusBox.vue' // 自定义盒子组件，可以显示内容或输入框
+import CusButton from '@/ui/CusButton.vue' // 自定义按钮组件
+import CusButton2 from '@/ui/CusButton2.vue'
+import CusColumn from '@/ui/CusColumn.vue' // 自定义列组件，显示选项菜单
+import CusCheckbox from '@/ui/CusCheckbox.vue' // 自定义复选框组件
+import User from '@/assets/User.png' // 引入用户图片
+import { getFeedback } from '@/apis/src/feedback'
+
+
+export default {
+  props:{
+    id: String 
+  },
+  // 注册上述引入的组件，供模板使用
+  components: {
+    CusBox,
+    CusButton,
+    CusButton2,
+    CusColumn,
+    CusCheckbox
+  },
+  data() {
+    // 定义数据 user，存储图片路径，供输入栏组件使用
+    return {
+      user: User,
+      loading: true,
+      data: {
+        "username": "",
+        "created_at": "",
+        "title": "",
+        "description": "",
+        "category": 0,
+        "urgency": 0,
+        "grade": 0,
+        "grade_content": "",
+        "undertaker": "",
+        "status": false,
+      },
+      reply: [],
+      rate: 5
+    }
+  },
+  mounted() {
+    if (this.$route.query.id)
+      getFeedback(this.$route.query.id).then(res=>{
+        this.data = res.data.data.request
+        this.reply = res.data.data.replies
+        this.loading = false
+        console.log(this.data)
+      })
+    else
+      this.$router.push({name:"view"})
+  },
+  computed: {
+    replies: {
+      get(this) :string{
+        if (this.reply == null)
+          return "无"
+        let s = ""
+        for (let i of this.reply){
+          s += i.respondent+": "+i.content+"\n"
+        }
+        return s
+      }
+    }
+  }
+}
+</script>
+
 <template>
-    <header>
+  <header>
      <!-- 左侧菜单栏 -->
      <cus-column content="内容" :items="[
        {title: '未处理反馈', select: () => {}}, 
@@ -10,170 +81,135 @@
        {title: '管理员管理', select: () => {}}
      ]" :default="1"></cus-column>
    </header>
-     
- 
-   <!-- 右侧表格 -->
-   <div style="position: absolute; left: 400px; top: 70px">
-     <div style="display: flex">
-       <cus-box :input="false" h="60px" w="845px" :textAlign="['center', 'top']">
-         <cus-button2 style="left: 10%; top: 19.3px" content="接单"></cus-button2>
-         <cus-button2 style="left: 42%; top: 19.3px" content="取消接单"></cus-button2>
-         <cus-button2 style="left: 80%; top: 19.3px" content="垃圾信息"></cus-button2>
-       </cus-box>
-     </div>
-     <!-- 第一行：标题 -->
-     <div style="display: flex">
-       <cus-box
-         :input="false"
-         content="标题"
-         h="60px"
-         w="141px"
-         :textAlign="['center', 'top']"
-       ></cus-box>
-       <cus-box
-         :input="false"
-         content="显示标题"
-         h="60px"
-         w="704px"
-         :textAlign="['center', 'top']"
-       ></cus-box>
-     </div>
- 
-     <!-- 第二行：是否紧急等 -->
-     <div style="display: flex">
-       <cus-box :input="false" h="60px" w="845px">
-         <cus-switch content="是否紧急" style="left: 10px; top: 19.3px"></cus-switch>
-         <!-- <cus-button2 content="紧急度" style="left: 150px;top:20.5px;"></cus-button2>
-           -->
-         <span style="position: absolute; left: 150px; top: 20.5px">紧急度</span>
-         <cus-checkbox
-           v-for="(index, i) in 5"
-           :key="i"
-           :content="index"
-           :style="{ position: 'absolute', left: 215 + i * 50 + 'px', top: '22px' }"
-         ></cus-checkbox>
-         <cus-switch content="是否匿名" style="left: 500px; top: 19.3px"></cus-switch>
-         <cus-switch content="是否处理" style="left: 650px; top: 19.3px"></cus-switch>
-       </cus-box>
-     </div>
- 
-     <!-- 第三行：问题类别 -->
-     <div style="display: flex">
-       <cus-box
-         :input="false"
-         content="问题类别"
-         h="60px"
-         w="141px"
-         :textAlign="['center', 'top']"
-       ></cus-box>
- 
-       <!-- 添加复选框行 -->
-       <div style="position: relative; width: 704px">
-         <cus-box :input="false" content="" h="60px" w="704px" :textAlign="['left', 'top']">
-           <cus-checkbox content="学术与课程" style="left: 0px; top: 19.3px"></cus-checkbox>
-           <cus-checkbox content="财务与奖学金" style="left: 115px; top: 19.3px"></cus-checkbox>
-           <cus-checkbox content="餐饮与住宿" style="left: 240px; top: 19.3px"></cus-checkbox>
-           <cus-checkbox content="技术与设施" style="left: 355px; top: 19.3px"></cus-checkbox>
-           <cus-checkbox content="安全与行政" style="left: 470px; top: 19.3px"></cus-checkbox>
-           <cus-checkbox content="其他" style="left: 585px; top: 19.3px"></cus-checkbox>
-         </cus-box>
-       </div>
-     </div>
- 
-     <!-- 第四行：内容 -->
-     <div style="display: flex">
-       <cus-box
-         :input="false"
-         content="内容"
-         h="109px"
-         w="141px"
-         :textAlign="['center', 'top']"
-       ></cus-box>
-       <cus-box
-         :input="false"
-         content="显示内容"
-         h="109px"
-         w="704px"
-         :textAlign="['left', 'top']"
-       ></cus-box>
-     </div>
- 
-     <!-- 第五行：照片 -->
-     <div style="display: flex">
-       <cus-box
-         :input="false"
-         content="照片"
-         h="70px"
-         w="141px"
-         :textAlign="['center', 'top']"
-       ></cus-box>
-       <cus-box
-         :input="false"
-         content="显示图片缩略图"
-         h="70px"
-         w="704px"
-         :textAlign="['left', 'top']"
-       ></cus-box>
-     </div>
- 
-     <!-- 第六行：回复 -->
-     <div style="display: flex">
-       <cus-box
-         :input="false"
-         content="回复"
-         h="144px"
-         w="141px"
-         :textAlign="['center', 'top']"
-       ></cus-box>
-       <cus-box
-         :input="true"
-         content="请输入回复"
-         h="144px"
-         w="704px"
-         :textAlign="['left', 'top']"
-       ></cus-box>
-     </div>
-     <cus-button content="提交评价" style="position: absolute; top: 600px; left: 268px"></cus-button>
-     <div style="display: flex">
-       <cus-box :input="false" h="80px" w="845px" :textAlign="['center', 'top']">
-         <cus-button style="left: 30%; top: 19.3px" content="提交"></cus-button>
-       </cus-box>
-     </div>
-   </div>
- 
-   <RouterView />
- </template>
- 
- <script>
- // 引入自定义组件和图片资源
- import CusBox from '@/ui/CusBox.vue'
- import CusButton from '@/ui/CusButton.vue'
- import CusButton2 from '@/ui/CusButton2.vue'
- import CusColumn from '@/ui/CusColumn.vue'
- import CusInput from '@/ui/CusInput.vue'
- import CusSwitch from '@/ui/CusSwitch.vue'
- import CusCheckbox from '@/ui/CusCheckbox.vue'
- import User from '@/assets/User.png' // 引入用户图片
- 
- export default {
-   components: {
-     CusBox,
-     CusButton,
-     CusButton2,
-     CusColumn,
-     CusInput,
-     CusSwitch,
-     CusCheckbox
-   },
-   data() {
-     return {
-       user: User
-     }
-   }
- }
- </script>
- 
- <style scoped>
- /* 去除列与列之间的间隙 */
- </style>
- 
+
+  <!-- 右侧表格 -->
+  <div style="position: absolute; left: 400px; top: 70px">
+    <div style="display: flex">
+      <cus-box :input="false" h="60px" w="845px" :textAlign="['center', 'top']">
+        <cus-button2 style="left: 10%; top: 19.3px" content="接单"></cus-button2>
+        <cus-button2 style="left: 42%; top: 19.3px" content="取消接单"></cus-button2>
+        <cus-button2 style="left: 80%; top: 19.3px" content="垃圾信息"></cus-button2>
+      </cus-box>
+    </div>
+    <!-- 第一行：标题 -->
+    <div style="display: flex">
+      <cus-box
+        :input="false"
+        content="标题"
+        h="60px"
+        w="141px"
+        :textAlign="['center', 'top']"
+      ></cus-box>
+      <cus-box
+        :input="false"
+        :content="data.title"
+        h="60px"
+        w="704px"
+        :textAlign="['center', 'top']"
+      ></cus-box>
+    </div>
+
+    <!-- 第二行：是否紧急等 -->
+    <div style="display: flex">
+      <cus-box :input="false" h="60px" w="845px" :textAlign="['left', 'top']">
+        <div class="info">
+          <span style="left: 6%">紧急度</span>
+          <CusCheckbox style="left: 18%" content="5" value="5" v-model:group="data.urgency"></CusCheckbox>
+          <CusCheckbox style="left: 27%" content="4" value="4" v-model:group="data.urgency"></CusCheckbox>
+          <CusCheckbox style="left: 36%" content="3" value="3" v-model:group="data.urgency"></CusCheckbox>
+          <CusCheckbox style="left: 45%" content="2" value="2" v-model:group="data.urgency"></CusCheckbox>
+          <CusCheckbox style="left: 54%" content="1" value="1" v-model:group="data.urgency"></CusCheckbox>
+          <CusCheckbox style="left: 63%" content="0" value="0" v-model:group="data.urgency"></CusCheckbox>
+          <CusCheckbox style="left: 72%" content="是否匿名" :readonly="true" :radio="false" :set="data.username=='匿名用户'"></CusCheckbox>
+          <CusCheckbox style="left: 85%" content="是否处理" :readonly="true" :radio="false" :set="data.status"></CusCheckbox>
+        </div>
+      </cus-box>
+    </div>
+
+    <!-- 第三行：问题类别 -->
+    <div style="display: flex">
+      <cus-box
+          :input="false"
+          content="问题类别"
+          h="60px"
+          w="141px"
+          :textAlign="['center', 'top']"
+        ></cus-box>
+        <cus-box :input="false" h="60px" w="704px" :textAlign="['left', 'top']">
+          <div class="info">
+            <CusCheckbox style="left: 2%" content="学术与课程" :readonly="true" :value="0" v-model:group="data.category"></CusCheckbox>
+            <CusCheckbox style="left: 19%" content="财务与奖学金" :readonly="true" :value="1" v-model:group="data.category"></CusCheckbox>
+            <CusCheckbox style="left: 38%" content="餐饮与住宿" :readonly="true" :value="2" v-model:group="data.category"></CusCheckbox>
+            <CusCheckbox style="left: 56%" content="技术与设施" :readonly="true" :value="3" v-model:group="data.category"></CusCheckbox>
+            <CusCheckbox style="left: 73%" content="安全与行政" :readonly="true" :value="4" v-model:group="data.category"></CusCheckbox>
+            <CusCheckbox style="left: 90%" content="其他" :readonly="true" :value="5" v-model:group="data.category"></CusCheckbox>
+          </div>
+        </cus-box>
+      </div>
+
+    <!-- 第四行：内容 -->
+    <div style="display: flex">
+      <cus-box
+        :input="false"
+        content="内容"
+        h="109px"
+        w="141px"
+        :textAlign="['center', 'top']"
+      ></cus-box>
+      <cus-box
+        :input="false"
+        :content="data.description"
+        h="109px"
+        w="704px"
+        :textAlign="['left', 'top']"
+      ></cus-box>
+    </div>
+
+    <!-- 第五行：照片 -->
+    <div style="display: flex">
+      <cus-box
+        :input="false"
+        content="照片"
+        h="70px"
+        w="141px"
+        :textAlign="['center', 'top']"
+      ></cus-box>
+      <cus-box
+        :input="false"
+        content="显示图片缩略图"
+        h="70px"
+        w="704px"
+        :textAlign="['left', 'top']"
+      ></cus-box>
+    </div>
+
+    <!-- 第六行：回复 -->
+    <div style="display: flex">
+      <cus-box
+        :input="false"
+        content="回复"
+        h="144px"
+        w="141px"
+        :textAlign="['center', 'top']"
+      ></cus-box>
+      <cus-box
+        :input="true"
+        content="请输入回复"
+        h="144px"
+        w="704px"
+        :textAlign="['left', 'top']"
+      ></cus-box>
+    </div>
+    <div style="display: flex">
+      <cus-box :input="false" h="80px" w="845px" :textAlign="['center', 'top']">
+        <cus-button style="left: 30%; top: 19.3px" content="提交"></cus-button>
+      </cus-box>
+    </div>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+/* 去除列与列之间的间隙 */
+</style>
